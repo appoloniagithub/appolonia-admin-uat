d70298d290963df1c6bfe053ef171b095dfba981
appolonia <116258461+appoloniagithub@users.noreply.github.com>
2022-12-09 22:07:33 +0400

    Update Login.js

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,163 +1,41 @@
-import PropTypes from "prop-types"
-import React, { Component } from "react"
-import { withRouter, Link } from "react-router-dom"
-import profile from "assets/images/profile-img.png"
-import applogo from "../../assets/images/applogo.png"
+import React from "react"
 import { Redirect } from "react-router-dom"
 
-import {
-  Row,
-  Col,
-  CardBody,
-  Card,
-  Alert,
-  Container,
-  Form,
-  Input,
-  FormFeedback,
-  Label,
-} from "reactstrap"
+// Profile
+import UserProfile from "../pages/Authentication/user-profile"
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props)
-    let loggedIn = false
-    this.state = {
-      email: "",
-      password: "",
-      loggedIn,
-    }
+// Authentication related pages
+import Login from "../pages/Authentication/Login"
+import Logout from "../pages/Authentication/Logout"
+import Register from "../pages/Authentication/Register"
+import ForgetPwd from "../pages/Authentication/ForgetPassword"
 
-    this.onChange = this.onChange.bind(this)
-    this.submitForm = this.submitForm.bind(this)
-  }
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    })
-  }
-  submitForm(e) {
-    e.preventDefault()
-    const { email, password } = this.state
+// Dashboard
+import Dashboard from "../pages/Dashboard/index"
+import Showpatient from "../pages/Patients/Showpatient"
+import Allpatients from "../pages/Patients/index"
 
-    if (email === "admin@appolonia.ae" && password === "123456") {
-      this.setState({
-        loggedIn: true,
-      })
-    }
-  }
+const authProtectedRoutes = [
+  { path: "/dashboard", component: Dashboard },
+  { path: "/patients", component: Allpatients },
 
-  render() {
-    if (this.state.loggedIn) {
-      return <Redirect to="/dashboard" />
-    }
-    return (
-      <React.Fragment>
-        <div className="home-btn d-none d-sm-block">
-          <Link to="/" className="text-dark">
-            <i className="fas fa-home h2" />
-          </Link>
-        </div>
-        <div className="account-pages my-5 pt-sm-5">
-          <Container>
-            <Row className="justify-content-center">
-              <Col md={8} lg={6} xl={5}>
-                <Card className="overflow-hidden">
-                  <div className="bg-primary bg-soft">
-                    <Row>
-                      <Col xs={7}>
-                        <div className="text-primary p-4">
-                          <h5 className="text-primary">
-                            Appolonia Dental Care
-                          </h5>
-                          <p>Sign in to Doctor Portal.</p>
-                        </div>
-                      </Col>
-                      <Col className="col-5 align-self-end">
-                        <img src={profile} alt="" className="img-fluid" />
-                      </Col>
-                    </Row>
-                  </div>
-                  <CardBody className="pt-0">
-                    <div>
-                      <Link to="/" className="auth-logo-light">
-                        <div className="avatar-md profile-user-wid mb-4">
-                          <span
-                            style={{ backgroundColor: "#20507B" }}
-                            className="avatar-title rounded-circle "
-                          >
-                            <img
-                              src={applogo}
-                              alt=""
-                              className=""
-                              height="34"
-                            />
-                          </span>
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="p-2">
-                      <Form
-                        onSubmit={this.submitForm}
-                        className="form-horizontal"
-                      >
-                        <div className="mb-3">
-                          <Label className="form-label">Email</Label>
-                          <Input
-                            name="email"
-                            className="form-control"
-                            placeholder="Enter email"
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.onChange}
-                          />
-                        </div>
+  // //profile
+  { path: "/profile", component: UserProfile },
 
-                        <div className="mb-3">
-                          <Label className="form-label">Password</Label>
-                          <Input
-                            name="password"
-                            type="password"
-                            placeholder="Enter Password"
-                            value={this.state.password}
-                            onChange={this.onChange}
-                          />
-                        </div>
+  // this route should be at the end of all other routes
+  // eslint-disable-next-line react/display-name
+  {
+    path: "/login",
+    exact: true,
+    component: () => <Redirect to="/dashboard" />,
+  },
+]
 
-                        <div className="form-check">
-                          <input
-                            type="checkbox"
-                            className="form-check-input"
-                            id="customControlInline"
-                          />
-                          <label
-                            className="form-check-label"
-                            htmlFor="customControlInline"
-                          >
-                            Remember me
-                          </label>
-                        </div>
+const publicRoutes = [
+  { path: "/logout", component: Logout },
+  { path: "/", component: Login },
+  { path: "/forgot-password", component: ForgetPwd },
+  { path: "/register", component: Register },
+]
 
-                        <div className="mt-3 d-grid">
-                          <button
-                            className="btn btn-primary btn-block"
-                            type="submit"
-                          >
-                            Log In
-                          </button>
-                        </div>
-                      </Form>
-                    </div>
-                  </CardBody>
-                </Card>
-                <div className="mt-5 text-center">
-                  <p>© {new Date().getFullYear()} Appolonia Dental Care </p>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </React.Fragment>
-    )
-  }
-}
+export { publicRoutes, authProtectedRoutes }
